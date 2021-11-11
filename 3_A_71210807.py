86ade63ae7e0a6a32e1e0029fc50ad1e4e688420
BOB EHUD RIFAEL GULTOM <bob.ehud@ti.ukdw.ac.id>
2021-11-11 18:57:48 +0700

    3_A_71210807.py

---
+++ 3_A_71210807.py
@@ -0,0 +1,7 @@
+item_1=input("Masukkan daftar belanja 1: ")
+item_2=input("Masukkan daftar belanja 2: ")
+Item_1=input("Tambahkan data ke daftar belanja 1: ")
+Item_2=input("Tambahkan data ke daftar belanja 2: ")
+print("Daftar belanja 1 adalah",item_1,Item_1)
+print("Daftar belanja 2 adalah",item_2,Item_2)
+
